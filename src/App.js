c8f015df5d72a43f445054125de171c9be268e11
Ajay Maurya <97455068+Ajay-Maury@users.noreply.github.com>
2022-06-14 19:43:17 +0530

    Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [count,setCount] = useState(0)
   return (
     <div className="App">
-      <h2 data-testid="counter-value">X</h2>
-      <button data-testid="counter-decrement-button"></button>
-      <button data-testid="counter-increment-button"></button>
+      <h2 data-testid="counter-value">{count}</h2>
+      <button data-testid="counter-decrement-button"
+      disabled={count==0}
+        onClick={() => count>0 && setCount(count - 1)}>Decrement</button>
+      <button data-testid="counter-increment-button" onClick={()=>setCount(count+1)}>Increment</button>
     </div>
   );
 }
